72bef652d2446589a2e7b4f462549ea8bdc35563
Theo McCaie <tam203@gmail.com>
2019-04-26 09:49:17 +0100

    fix bugs in dataload so that path names correct. Added highlighting and scrolling to henry added cell. stop henry exicuting the cell for speed

--- src/localintenthandler.ts
+++ src/localintenthandler.ts
@@ -1,7 +1,8 @@
 import { IntentHandler, HenryIntent, Intent, DataLoadIntent } from "./intents";
 import { DocumentRegistry } from "@jupyterlab/docregistry";
-import { INotebookModel, NotebookPanel, NotebookActions } from "@jupyterlab/notebook";
+import { INotebookModel, NotebookPanel /*, NotebookActions */ } from "@jupyterlab/notebook";
 import { findCubes } from "./datacat";
+import { Cell } from "@jupyterlab/cells";
 export class LocalIntentHandler implements IntentHandler {
     handelIntent(intent: HenryIntent, notebookPanel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): Promise<string> {
         console.log(intent);
@@ -19,16 +20,23 @@ export class LocalIntentHandler implements IntentHandler {
 }
 
 function handelLoadDate(intent: DataLoadIntent, nbp: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): string {
+
+    if (!intent.dataset && !intent.param) {
+        return 'Sorry, I didn\'t catch what data and model you wanted. Try saying something like "load rain data from ukv".'
+    }
+
     let nb = nbp.content
     let cubes = findCubes(intent.param, intent.dataset)
 
-
-    if (!cubes) {
+    if (cubes.length <= 0) {
         return `Sorry, I couldn't find data for ${intent.param} in ${intent.dataset}.`
+    }
+    if (cubes.length > 10) {
+        return `Sorry, I found more than 10 datasets that match param:${intent.param} and model:${intent.dataset}. Can you be more precise?`
     }
 
     let code = "import intake\n"
-    code += `henry_loaded_data = [` + cubes.map(cube => `intake.cat.${cube.key}.read()`).join(',') + `]\n`
+    code += `henry_loaded_data = [\n    ` + cubes.map(cube => `intake.cat.${cube.key}.read()`).join(',\n    ') + `\n]\n`
     code += "henry_loaded_data"
 
     let msg = `I hope this helps... I loaded ${intent.param} from ${intent.dataset}`;
@@ -37,6 +45,27 @@ function handelLoadDate(intent: DataLoadIntent, nbp: NotebookPanel, context: Doc
     nb.model.cells.insert(insertAt, cell)
     cell.value.text = code
     nb.activeCellIndex = insertAt
-    NotebookActions.run(nb, context.session)
+    nb.scrollToPosition(100)
+
+
+    // NotebookActions.run(nb, context.session) // Let's not run the code but we could!
+
+    let cellWidget = nb.widgets[insertAt];
+
+    nb.scrollToPosition(cellWidget.node.offsetTop, 20)
+    cellWidget.node.classList.add('henry-cell')
+    flashCell(cellWidget, 10)
     return msg
+}
+
+function flashCell(cell: Cell, count: number) {
+    count -= 1
+    if (count <= 0) {
+        cell.node.classList.remove('henry-flash')
+        return // exit early, all flashed out
+    }
+
+    cell.node.classList.toggle('henry-flash')
+    setTimeout(() => { flashCell(cell, count) }, 500)
+
 }
